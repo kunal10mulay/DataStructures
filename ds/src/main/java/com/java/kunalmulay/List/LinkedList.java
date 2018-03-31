68f30a8d5b0eaaa77590be7a5c0e68f3cfc6875a
kunal10mulay <kunal10mulay@gmail.com>
2018-03-31 23:09:47 +0530

    nthfromLast
    add nth from last function

--- ds/src/main/java/com/java/kunalmulay/List/LinkedList.java
+++ ds/src/main/java/com/java/kunalmulay/List/LinkedList.java
@@ -3,40 +3,42 @@ package com.java.kunalmulay.List;
 import javax.management.RuntimeErrorException;
 
 /**
- * A Single Linked list implementation and some operations
- * on this list. 
+ * A Single Linked list implementation and some operations on this list.
+ * 
  * @author kunal mulay
  *
  * @param <T>
  */
 public class LinkedList<T> implements List<T> {
 	private Node<T> head;
-	
+
 	/**
-	 * A linked list object can be create by providing
-	 * a starting or head node. 
+	 * A linked list object can be create by providing a starting or head node.
+	 * 
 	 * @param node
 	 */
-	public LinkedList(Node<T> node){
+	public LinkedList(Node<T> node) {
 		this.head = node;
 	}
-	
+
 	/**
 	 * Default constructor where head is null.
 	 */
-	public LinkedList(){
+	public LinkedList() {
 	}
-	
+
 	/**
 	 * Get head of a linked List.
+	 * 
 	 * @return
 	 */
 	public Node<T> getHead() {
 		return head;
 	}
-	
+
 	/**
 	 * Set head of a linked List.
+	 * 
 	 * @return
 	 */
 	public void setHead(Node<T> head) {
@@ -49,54 +51,51 @@ public class LinkedList<T> implements List<T> {
 			head = node;
 		else {
 			Node<T> current = head;
-			while(current.next != null) {
+			while (current.next != null) {
 				current = current.next;
 			}
 			current.next = node;
 		}
 	}
-	
+
 	public void display() {
 		Node<T> current = head;
-		while(current.next != null) {
+		while (current.next != null) {
 			System.out.print(current.data + " : ");
 			current = current.next;
 		}
 		System.out.println(current.data);
 	}
-	
+
 	public String displayString() {
 		Node<T> current = head;
 		StringBuffer buffer = new StringBuffer();
-		while(current.next != null) {
+		while (current.next != null) {
 			buffer.append(current.data + " : ");
 			current = current.next;
 		}
 		buffer.append(current.data);
 		return buffer.toString();
 	}
-	
+
 	@Override
-	public Node<T> addNodeAtLocation(Node<T> node, int location){
+	public Node<T> addNodeAtLocation(Node<T> node, int location) {
 		Node<T> current = getHead();
 		int count = 1;
-		if(current == null && location == 0) {
+		if (current == null && location == 0) {
 			setHead(node);
 			node.next = current;
-		}
-		else if(current == null && location != 0){
-			throw new RuntimeException("Could not add a node to location " + location +
-					 " because list is empty" );
-		}
-		else {
-			while(current.next != null && count<location) {
+		} else if (current == null && location != 0) {
+			throw new RuntimeException("Could not add a node to location " + location + " because list is empty");
+		} else {
+			while (current.next != null && count < location) {
 				count++;
 				current = current.next;
 			}
-			if(location > count)
+			if (location > count)
 				throw new RuntimeException("List out of index");
 			else {
-				Node <T> temp = current.next;
+				Node<T> temp = current.next;
 				current.next = node;
 				node.next = temp;
 			}
@@ -106,21 +105,21 @@ public class LinkedList<T> implements List<T> {
 
 	@Override
 	public void appendList(List<T> list) {
-		Node <T> current = getHead();
-		while(current.next != null)
+		Node<T> current = getHead();
+		while (current.next != null)
 			current = current.next;
-		if(list instanceof LinkedList ) {
+		if (list instanceof LinkedList) {
 			current.next = ((LinkedList<T>) list).getHead();
-		}else {
+		} else {
 			throw new RuntimeException("Incompatible lists provided");
-		}	
+		}
 	}
 
 	@Override
 	public boolean contains(Node<T> node) {
-		Node <T> current = getHead();
-		while(current.next != null) {
-			if(node.equals(current))
+		Node<T> current = getHead();
+		while (current.next != null) {
+			if (node.equals(current))
 				return true;
 			current = current.next;
 		}
@@ -129,15 +128,15 @@ public class LinkedList<T> implements List<T> {
 
 	@Override
 	public Node<T> get(int index) {
-		if(index < 0)
+		if (index < 0)
 			throw new RuntimeException("Index out of bound");
 		int count = 0;
 		Node<T> current = getHead();
 		while (current != null && count < index) {
-			count ++;
+			count++;
 			current = current.next;
 		}
-		if(index == count)
+		if (index == count)
 			return current;
 		else
 			throw new RuntimeException("Index out of bound");
@@ -145,41 +144,40 @@ public class LinkedList<T> implements List<T> {
 
 	@Override
 	public void remove(int index) {
-		if(index < 0)
+		if (index < 0)
 			throw new RuntimeException("Index out of bound");
 		int count = 1;
 		Node<T> current = getHead();
 		Node<T> previous = null;
-		if(index == 0 && current != null)  {
+		if (index == 0 && current != null) {
 			setHead(current.next);
-		} else if(index == 0 && current == null)
+		} else if (index == 0 && current == null)
 			throw new RuntimeException("Index out of bound");
 		else {
-			while(current != null) {
+			while (current != null) {
 				previous = current;
 				current = current.next;
-				if(index == count) {
+				if (index == count) {
 					previous.next = current.next;
-				}
-				else if(count > index) {
+				} else if (count > index) {
 					throw new RuntimeException("Index out of bound");
 				}
-				count ++;
+				count++;
 			}
 		}
 	}
 
 	@Override
 	public void remove(Node<T> node) {
-		Node <T> current = getHead();
-		Node <T> previous = null;
-		if(current.equals(node)) {
+		Node<T> current = getHead();
+		Node<T> previous = null;
+		if (current.equals(node)) {
 			setHead(current.next);
 			return;
 		}
-		while(current != null) {
-			if(current.equals(node)) {
-				previous.next =current.next;
+		while (current != null) {
+			if (current.equals(node)) {
+				previous.next = current.next;
 				return;
 			}
 			previous = current;
@@ -191,10 +189,36 @@ public class LinkedList<T> implements List<T> {
 	public int length() {
 		int count = 0;
 		Node<T> current = getHead();
-		while(current != null) {
+		while (current != null) {
 			current = current.next;
 			count++;
 		}
 		return count;
+	}
+	
+	/**
+	 * Find nth node from the end of linked list
+	 * @return nth node from end
+	 */
+	public Node<T> nthFromEnd(int n){
+		// Nth node from end is m-nth node from start, where m is length of 
+		// List. To solve this problem in single iteration, take two pointers,
+		// First pointer moves until it reaches n, then we start second pointer
+		// until end of first List. The current node pointed by ptr2 is m-nth Node.
+		
+		Node<T> ptr1 = getHead();
+		Node<T> ptr2 = getHead();
+		while (n > 0 && ptr1!=null) {
+			ptr1 = ptr1.next;
+			n--;
+		}
+		if(ptr1 == null && n > 1) {
+			throw new RuntimeException("Index out of bound");
+		}
+		while(ptr1!=null) {
+			ptr2=ptr2.next;
+			ptr1=ptr1.next;
+		}
+		return ptr2;
 	}
 }
--- ds/src/test/java/com/java/kunalmulay/test/List/ListTests.java
+++ ds/src/test/java/com/java/kunalmulay/test/List/ListTests.java
@@ -9,145 +9,161 @@ import com.java.kunalmulay.List.Node;
 
 public class ListTests {
 
-	 @Test
-	 public void addAndDisplayLinkedList() {
-	       LinkedList<String> linkedList = new LinkedList<>();
-	       linkedList.add(new Node<String>("A"));
-	       linkedList.add(new Node<String>("B"));
-	       linkedList.add(new Node<String>("C"));
-	       linkedList.add(new Node<String>("D"));
-	       
-	        // assert statement
-	        assertEquals(linkedList.displayString(), "A : B : C : D");
-	    }
-	 
-	 @Test
-	 public void addToListAtLocation() {
-		   LinkedList<String> linkedList = new LinkedList<>();
-	       linkedList.addNodeAtLocation(new Node<String>("A"), 0);
-	       linkedList.addNodeAtLocation(new Node<String>("C"), 1);
-	       linkedList.addNodeAtLocation(new Node<String>("B"), 1);
-	       linkedList.add(new Node<String>("D"));
-	       linkedList.addNodeAtLocation(new Node<String>("E"), 4);
-	       
-	       assertEquals(linkedList.displayString(), "A : B : C : D : E");
-	 }
-	 
-	 @Test
-	 public void appendList() {
-		 LinkedList<String> linkedList = new LinkedList<>();
-		 linkedList.add(new Node<String>("A"));
-	     linkedList.add(new Node<String>("B"));
-	     linkedList.add(new Node<String>("C"));
-	     
-		 LinkedList<String> linkedList2 = new LinkedList<>();
-		 linkedList2.add(new Node<String>("D"));
-	     linkedList2.add(new Node<String>("E"));
-	     linkedList2.add(new Node<String>("F"));
-	     
-	     linkedList.appendList(linkedList2);
-	     assertEquals(linkedList.displayString(), "A : B : C : D : E : F");
-	 }
-	 
-	 @Test
-	 public void containsList() {
-		 LinkedList<String> linkedList = new LinkedList<>();
-		 linkedList.add(new Node<String>("A"));
-	     linkedList.add(new Node<String>("B"));
-	     linkedList.add(new Node<String>("C"));
-		 linkedList.add(new Node<String>("D"));
-	     linkedList.add(new Node<String>("E"));
-	     
-	     assertEquals(linkedList.contains(new Node<String>("C")), true);
-	     assertEquals(linkedList.contains(new Node<String>("F")), false);
-	 }
-	 
-	 @Test
-	 public void getFromList() {
-		 LinkedList<String> linkedList = new LinkedList<>();
-		 linkedList.add(new Node<String>("A"));
-	     linkedList.add(new Node<String>("B"));
-	     linkedList.add(new Node<String>("C"));
-		 linkedList.add(new Node<String>("D"));
-	     linkedList.add(new Node<String>("E"));
-	     
-	     assertTrue(linkedList.get(2).equals(new Node<String> ("C")));
-	     
-	     // assert true if exception is produced
-	     try {
-	    	 linkedList.get(10);
-	     }catch (RuntimeException e) {
+	@Test
+	public void addAndDisplayLinkedList() {
+		LinkedList<String> linkedList = new LinkedList<>();
+		linkedList.add(new Node<String>("A"));
+		linkedList.add(new Node<String>("B"));
+		linkedList.add(new Node<String>("C"));
+		linkedList.add(new Node<String>("D"));
+
+		// assert statement
+		assertEquals(linkedList.displayString(), "A : B : C : D");
+	}
+
+	@Test
+	public void addToListAtLocation() {
+		LinkedList<String> linkedList = new LinkedList<>();
+		linkedList.addNodeAtLocation(new Node<String>("A"), 0);
+		linkedList.addNodeAtLocation(new Node<String>("C"), 1);
+		linkedList.addNodeAtLocation(new Node<String>("B"), 1);
+		linkedList.add(new Node<String>("D"));
+		linkedList.addNodeAtLocation(new Node<String>("E"), 4);
+
+		assertEquals(linkedList.displayString(), "A : B : C : D : E");
+	}
+
+	@Test
+	public void appendList() {
+		LinkedList<String> linkedList = new LinkedList<>();
+		linkedList.add(new Node<String>("A"));
+		linkedList.add(new Node<String>("B"));
+		linkedList.add(new Node<String>("C"));
+
+		LinkedList<String> linkedList2 = new LinkedList<>();
+		linkedList2.add(new Node<String>("D"));
+		linkedList2.add(new Node<String>("E"));
+		linkedList2.add(new Node<String>("F"));
+
+		linkedList.appendList(linkedList2);
+		assertEquals(linkedList.displayString(), "A : B : C : D : E : F");
+	}
+
+	@Test
+	public void containsList() {
+		LinkedList<String> linkedList = new LinkedList<>();
+		linkedList.add(new Node<String>("A"));
+		linkedList.add(new Node<String>("B"));
+		linkedList.add(new Node<String>("C"));
+		linkedList.add(new Node<String>("D"));
+		linkedList.add(new Node<String>("E"));
+
+		assertEquals(linkedList.contains(new Node<String>("C")), true);
+		assertEquals(linkedList.contains(new Node<String>("F")), false);
+	}
+
+	@Test
+	public void getFromList() {
+		LinkedList<String> linkedList = new LinkedList<>();
+		linkedList.add(new Node<String>("A"));
+		linkedList.add(new Node<String>("B"));
+		linkedList.add(new Node<String>("C"));
+		linkedList.add(new Node<String>("D"));
+		linkedList.add(new Node<String>("E"));
+
+		assertTrue(linkedList.get(2).equals(new Node<String>("C")));
+
+		// assert true if exception is produced
+		try {
+			linkedList.get(10);
+		} catch (RuntimeException e) {
 			assertTrue("Got exception", true);
 		}
-	 }
-	 
-	 @Test
-	 public void removeFromListByindex() {
-		 LinkedList<String> linkedList = new LinkedList<>();
-		 linkedList.add(new Node<String>("A"));
-	     linkedList.add(new Node<String>("B"));
-	     linkedList.add(new Node<String>("C"));
-		 linkedList.add(new Node<String>("D"));
-	     linkedList.add(new Node<String>("E"));
-	     linkedList.remove(0);
-	     
-	     assertEquals(linkedList.displayString(), "B : C : D : E");
-	     
-	     // assert true if exception is produced
-	     try {
-	    	 linkedList.remove(10);
-	     }catch (RuntimeException e) {
+	}
+
+	@Test
+	public void removeFromListByindex() {
+		LinkedList<String> linkedList = new LinkedList<>();
+		linkedList.add(new Node<String>("A"));
+		linkedList.add(new Node<String>("B"));
+		linkedList.add(new Node<String>("C"));
+		linkedList.add(new Node<String>("D"));
+		linkedList.add(new Node<String>("E"));
+		linkedList.remove(0);
+
+		assertEquals(linkedList.displayString(), "B : C : D : E");
+
+		// assert true if exception is produced
+		try {
+			linkedList.remove(10);
+		} catch (RuntimeException e) {
 			assertTrue("Got exception", true);
 		}
-	     
-	     try {
-	    	 new LinkedList<String>().remove(0);
-	     } catch (RuntimeException e) {
-				assertTrue("Got exception", true);
-			}
-	     
-	     try {
-	    	 new LinkedList<String>().remove(-1);
-	     } catch (RuntimeException e) {
-				assertTrue("Got exception", true);
-			}
-	 }
-	 
-	 @Test
-	 public void removeFromListByNode() {
-		 LinkedList<String> linkedList = new LinkedList<>();
-		 linkedList.add(new Node<String>("A"));
-	     linkedList.add(new Node<String>("B"));
-	     linkedList.add(new Node<String>("C"));
-		 linkedList.add(new Node<String>("D"));
-	     linkedList.add(new Node<String>("E"));
-	     linkedList.remove(new Node<String>("A"));
-	     
-	     assertEquals(linkedList.displayString(), "B : C : D : E");
-	     
-	     linkedList.remove(new Node<String>("C"));
-	     
-	     assertEquals(linkedList.displayString(), "B : D : E");
-	     
-	     linkedList.remove(new Node<String>("E"));
-	     
-	     assertEquals(linkedList.displayString(), "B : D");
-	 }
-	 
-	 @Test
-	 public void lengthOfList() {
-		 LinkedList<String> linkedList = new LinkedList<>();
-		 
-	     assertEquals(linkedList.length(), 0);
-		 
-		 linkedList.add(new Node<String>("A"));
-	     linkedList.add(new Node<String>("B"));
-	     linkedList.add(new Node<String>("C"));
-		 linkedList.add(new Node<String>("D"));
-	     linkedList.add(new Node<String>("E"));
-	     
-	     assertEquals(linkedList.length(), 5);
-	   
-	 }
-	 
+
+		try {
+			new LinkedList<String>().remove(0);
+		} catch (RuntimeException e) {
+			assertTrue("Got exception", true);
+		}
+
+		try {
+			new LinkedList<String>().remove(-1);
+		} catch (RuntimeException e) {
+			assertTrue("Got exception", true);
+		}
+	}
+
+	@Test
+	public void removeFromListByNode() {
+		LinkedList<String> linkedList = new LinkedList<>();
+		linkedList.add(new Node<String>("A"));
+		linkedList.add(new Node<String>("B"));
+		linkedList.add(new Node<String>("C"));
+		linkedList.add(new Node<String>("D"));
+		linkedList.add(new Node<String>("E"));
+		linkedList.remove(new Node<String>("A"));
+
+		assertEquals(linkedList.displayString(), "B : C : D : E");
+
+		linkedList.remove(new Node<String>("C"));
+
+		assertEquals(linkedList.displayString(), "B : D : E");
+
+		linkedList.remove(new Node<String>("E"));
+
+		assertEquals(linkedList.displayString(), "B : D");
+	}
+
+	@Test
+	public void lengthOfList() {
+		LinkedList<String> linkedList = new LinkedList<>();
+
+		assertEquals(linkedList.length(), 0);
+
+		linkedList.add(new Node<String>("A"));
+		linkedList.add(new Node<String>("B"));
+		linkedList.add(new Node<String>("C"));
+		linkedList.add(new Node<String>("D"));
+		linkedList.add(new Node<String>("E"));
+
+		assertEquals(linkedList.length(), 5);
+
+	}
+	
+	@Test
+	public void nthfromlast() {
+		LinkedList<String> linkedList = new LinkedList<>();
+
+		assertEquals(linkedList.length(), 0);
+
+		linkedList.add(new Node<String>("A"));
+		linkedList.add(new Node<String>("B"));
+		linkedList.add(new Node<String>("C"));
+		linkedList.add(new Node<String>("D"));
+		linkedList.add(new Node<String>("E"));
+
+		assertTrue(linkedList.nthFromEnd(5).equals(new Node<String>("A")));
+
+	}
+
 }
